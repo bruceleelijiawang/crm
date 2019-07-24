4c9b5de5bc6a20424bfac39d952006b89f7fd8db
bruceleelijiawang <1319522723@qq.com>
2019-07-24 10:03:44 +0800

    init crm project

---
+++ src/main/java/com/addidas/project/crmqpp/CrmQppApplication.java
@@ -0,0 +1,13 @@
+package com.addidas.project.crmqpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmQppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmQppApplication.class, args);
+	}
+
+}
