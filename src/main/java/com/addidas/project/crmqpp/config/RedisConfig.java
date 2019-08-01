fc2433e89286fe31ed66a1f9faf557e0a50f6286
bruceleelijiawang <1319522723@qq.com>
2019-08-01 17:08:42 +0800

    [缓存时间] 	缓存时间生效

--- src/main/java/com/addidas/project/crmqpp/config/RedisConfig.java
+++ src/main/java/com/addidas/project/crmqpp/config/RedisConfig.java
@@ -55,9 +55,11 @@ public class RedisConfig extends CachingConfigurerSupport{
 				RedisSerializer<Object> jsonSerializer = new GenericJackson2JsonRedisSerializer();
 				RedisSerializationContext.SerializationPair<Object> pair = RedisSerializationContext.SerializationPair.fromSerializer(jsonSerializer);
 				//--2.初始化配置
-				RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig().serializeValuesWith(pair);
+				RedisCacheConfiguration defaultCacheConfig = 
+						RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(30)).
+						serializeValuesWith(pair);
 				//-- 设置默认超过期时间是30秒
-				defaultCacheConfig.entryTtl(Duration.ofSeconds(30));
+		
 				//-- 初始化RedisCacheManager
 				return new RedisCacheManager(redisCacheWriter,defaultCacheConfig);
 
